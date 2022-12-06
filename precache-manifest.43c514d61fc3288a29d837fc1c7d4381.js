69be0f0ef3fba9db5e2b41f60162ceb0323f8afb
Parshuram Bagade <parshurambagade855@gmail.com>
2022-12-06 11:20:36 +0530

    Updates

---
+++ precache-manifest.43c514d61fc3288a29d837fc1c7d4381.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d78f4eedb5ffecf83e4e83940d2b807",
+    "url": "/keep-note/index.html"
+  },
+  {
+    "revision": "1eadfdca23b79cb9b9b7",
+    "url": "/keep-note/static/js/2.aa415f1b.chunk.js"
+  },
+  {
+    "revision": "a7892a6e329be86627e6",
+    "url": "/keep-note/static/js/main.0f84eb55.chunk.js"
+  },
+  {
+    "revision": "bba4f60c2247f7ef3dfd",
+    "url": "/keep-note/static/js/runtime-main.748b5fcf.js"
+  }
+]);
